97c28f2fa86058d1249b13029415a759ebb715fd
Lance Johnson <lancejohnson2009@gmail.com>
2022-04-06 18:17:49 -0500

    Finished Block 3 Complex Date Objects Labs

---
+++ Block3Lab/ComplexDataObjectLab.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Block3LabLibrary;
+
+namespace Block3Lab
+{
+    internal class ComplexDataObjectsLab
+    {
+        static void Main(string[] args)
+        {
+            Product p1 = new Product(123, "Cleaner", 4455, "Counter Top Disinfectant", 3.49m);
+            Console.WriteLine(p1);
+            Product p2 = new Product(230, "Toothpaste", 6454, "Minty Fresh", 5.29m);
+            Product p3 = new Product(111, "Broccoli", 1134, "Fresh Broccoli", 3.98m);
+            Product p4 = new Product(222, "Dog Toy", 1445, "Plush & Squeaky", 1.99m);
+            Product p5 = new Product(321, "Road Bike", 5873, "Carbon Fiber", 3500m);
+            Product p6 = new Product(908, "Binder", 52345, "Hardback Binder", 5.49m);
+
+            List<Product> products = new List<Product>();
+            products.Add(p1);
+            products.Add(p2);
+            products.Add(p3);
+            products.Add(p4);
+            products.Add(p5);
+            products.Add(p6);
+
+            foreach (var product in products)
+            {
+                Console.WriteLine("\n\n-=-=-=-=-=-=-=\n");
+                Console.WriteLine(product);
+            }
+
+
+            Console.WriteLine("\n\nDictonary Entry\n\n");
+
+            Dictionary<string, Product> pd2 = new Dictionary<string, Product>()
+            {
+                {p1.Name, p1 },
+                {p2.Name, p2 },
+                {p3.Name, p3 },
+                {p4.Name, p4 },
+                {p5.Name, p5 },
+                {p6.Name, p6 },
+
+            };
+
+            foreach (KeyValuePair<string, Product> kvp in pd2)
+            {
+                Console.WriteLine(kvp.Value);
+            }
+
+
+
+        }//end Main()
+    }//end Class
+}//end Namespace
---
+++ Block3LabLibrary/Product.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Block3LabLibrary
+{
+    public class Product
+    {
+        //FIELDS
+        private int _id;
+        private string _name;
+        private int _modelNumber;
+        private string _description;
+        private decimal _price;
+
+        //PROPERTIES
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public int ModelNumber
+        {
+            get { return _modelNumber; }
+            set { _modelNumber = value; }
+        }
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+        public decimal Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+        //CONSTRUCTORS
+
+        public Product(int id, string name, int modelNumber, string description, decimal price)
+        {
+            Id = id;
+            Name = name;
+            ModelNumber = modelNumber;
+            Description = description;
+            Price = price;
+
+        }
+
+        //METHODS
+        public override string ToString()
+        {
+            return string.Format($"Product ID: {Id}\nProduct Name: {Name}\nModel Number: {ModelNumber}\nDescription: {Description}\n" +
+                $"Price: {Price:c}");
+        }
+    }
+}
